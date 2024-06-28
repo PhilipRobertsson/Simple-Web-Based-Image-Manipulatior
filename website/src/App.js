e65976966a35443fb4249efd6edb76e2c3fb7a47
PhilipRobertsson <phiro138@student.liu.se>
2024-06-28 22:49:18 +0200

    Slide in menu added, no functionality in it yet

--- website/src/App.js
+++ website/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [selectedImage, setSelectedImage] = useState()
+  const [menuOut, setMenuOut] = useState(false)
   
   const handleFileChange = (e) =>{
     console.log(e.target.files)
@@ -11,6 +12,24 @@ function App() {
       setSelectedImage(URL.createObjectURL(e.target.files[0]))
     }else{
       console.log("No file chosen")
+    }
+  }
+
+  const handleMenuChange = () =>{
+    if(document.getElementById("Menu-window")){
+      if(menuOut){
+        setMenuOut(false);
+        console.log("Menu In");
+        document.getElementById("Menu-window").style.animation = "slideOutLeft 0.2s linear";
+        setTimeout(function() {
+          document.getElementById("Menu-window").style.display="none";
+        }, 190);
+      }else{
+        setMenuOut(true);
+        console.log("Menu Out");
+        document.getElementById("Menu-window").style.display="flex";
+        document.getElementById("Menu-window").style.animation = "slideInLeft 0.2s linear";
+      }
     }
   }
 
@@ -22,22 +41,59 @@ function App() {
       }else{
         imgContainer.style.display = "flex"
       }
+
+      function handleKeyDown(e){
+        console.log(e.code)
+        if(e.code == "Escape"){
+          handleMenuChange()
+        }
+      }
+
+      document.addEventListener('keydown', handleKeyDown);
+
+        //Remove event listener after use
+        return function cleanup() {
+          document.removeEventListener('keydown', handleKeyDown);
+        }
     }
-  },[selectedImage])
+  },[menuOut,selectedImage])
 
 
 
   return (
     <div className="App">
+
       <header className="App-header">
-        <h1>Title</h1>
+        <img id="Menu-button" src="menuIcon.svg" onClick={handleMenuChange}></img>
+        <h1 style={{padding:"20px"}}>Image Manipulator</h1>
       </header>
+
       <div className="App-content">
         <img id="Selected-image" src={selectedImage}/>
         <label className="Image-input">
           <input className="Image-input-field" type="file" accept="image/*" onChange={handleFileChange}></input>
           <a>Select image</a>
         </label>
+
+        <div className="Menus-container">
+          <div id="Menu-window">
+            <img className="Close-button" src="closeIcon.svg" onClick={handleMenuChange}></img>
+            <h3 style={{paddingLeft:"2em"}}>Menu</h3>
+            <div className='Menu-item'>
+                  <b>Dithering</b>
+                </div>
+                <div className='Menu-item'>
+                  <b>Blur</b>
+                </div>
+                <div className='Menu-item'>
+                  <b>Convertion</b>
+                </div>
+                <div className='Menu-item'>
+                  <b>Credits</b>
+                  <p>Created by Philip Robertsson</p>
+                </div>
+          </div>
+        </div>
       </div>
     </div>
   );
