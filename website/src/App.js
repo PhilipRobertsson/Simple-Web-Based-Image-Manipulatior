35c151a049e60538d3faa4140cba4b3f687d2433
PhilipRobertsson <phiro138@student.liu.se>
2024-06-28 18:13:21 +0200

    Image loading added

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
+import{useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState()
+  
+  const handleFileChange = (e) =>{
+    console.log(e.target.files)
+    setSelectedImage(URL.createObjectURL(e.target.files[0]))
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Title</h1>
       </header>
+      <div className="App-content">
+        <img className="Selected-image" src={selectedImage}/>
+        <label className="Image-input">
+          <input className="Image-input-field" type="file" accept="image/*" onChange={handleFileChange}></input>
+          <a>Select image</a>
+        </label>
+      </div>
     </div>
   );
 }
